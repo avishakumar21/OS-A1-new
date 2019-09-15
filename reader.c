3a2d6635e91f2b80bffeb440bb3783266bae55cb
Avisha Kumar <ak754@cornell.edu>
2019-09-15 18:14:47 -0400

    A1 reader implementation

--- reader.c
+++ reader.c
@@ -19,18 +19,36 @@
 
 struct reader {
 	int fd;
+	char buffer[512];
+	int size;
+	int index;
+
 };
 
 reader_t reader_create(int fd){
 	reader_t reader = (reader_t) calloc(1, sizeof(*reader));
 	reader->fd = fd;
+	reader->index = 0; //start at first index 
+	reader->size = 0; //initialize size to be zero
 	return reader;
 }
 
 char reader_next(reader_t reader){
-	char c;
-	int n = read(reader->fd, &c, 1);
-	return n == 1 ? c : EOF;
+	char c = reader->buffer;
+	//condition for if the index is one less than the end, or at the end
+	if(f (reader->index == reader->size || reader->index + 1 == reader->size))
+	{
+		//reset the index, get the size, return next character buffer or EOF 
+		reader->index = 0;
+		reader->size = read(reader->fd, &c, 512);
+		return reader->size != 0 ? c[0] : EOF;
+	}
+	else
+	{
+		//increment index, return next character
+		reader->index = reader_index + 1;
+		return c[reader->index];
+	}
 }
 
 void reader_free(reader_t reader){
